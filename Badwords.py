ffeb7f17c2bce3d7cda22edcf6679404b16c1203
Shermoond <57485579+Shermoond@users.noreply.github.com>
2020-01-21 16:10:07 +0200

    Create Badwords.py

---
+++ Badwords.py
@@ -0,0 +1,48 @@
+def checker(word):
+    y  = word
+    bad=["f","F","c","C","k","K","r","R"]
+    vowels=["a","A","e","E","i","I","o","O","u","U","y","Y"]
+    x=[]
+    for i in y :
+        check=1
+        for j in vowels:
+            if j==i:
+                check=0
+        if check==1:
+            x.append(i)
+    bc=0
+    gc=0
+    for i in x:
+        check = 1
+        for j in bad:
+            if i==j:
+                check=1
+                break
+            else:
+                check=0
+        print (check)
+        if check==1:
+            bc+=1
+
+        else:
+            gc+=1
+        
+    print (bc,gc)
+    if bc>=gc:
+        print ("bad  word")
+    else:
+        print("Good word")
+
+
+doc=open("test.txt","r")
+wordlist=doc.read()
+brokenup=wordlist.split()
+for l in brokenup:
+    if len(l)>3:
+        word=l
+        print(l)
+        checker(l)
+doc.close()
+
+
+
